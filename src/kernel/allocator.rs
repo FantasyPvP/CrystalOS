5fc14dbbecbd5a5565d3a001fb40982e25056393
FantasyPvP <harry.blaise.irving@gmail.com>
2023-02-23 00:52:15 +0000

    changed dependency versions
    updated some dependencies due to known security vulnerabilities

--- src/kernel/allocator.rs
+++ src/kernel/allocator.rs
@@ -31,7 +31,7 @@ pub fn init_heap(
 			mapper.map_to(page, frame, flags, frame_allocator)?.flush()
 		};
 
-		unsafe { ALLOCATOR.lock().init(HEAP_START, HEAP_SIZE); }
+		unsafe { ALLOCATOR.lock().init(HEAP_START as *mut u8, HEAP_SIZE); }
 	}
 	Ok(())
 }
